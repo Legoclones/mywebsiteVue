5882fc0328837c98229d7e2f3e5f00743bd598d1
Justin Applegate <justink.applegate@gmail.com>
2021-01-23 20:31:47 -0700

    Updated thoughts

--- src/data/thoughts.js
+++ src/data/thoughts.js
@@ -1,16 +1,28 @@
 let data = [
     {
+        question: 'What do you consider to be your greatest professional achievement?',
+        answer: 'My greatest professional achievement (up to this point) is ranking 3rd place in a Capture the Flag competition here at BYU. I had worked for a couple of months prior to obtain skills and gain practice with other CTF\'s so I would be properly prepared. When it started, I had my whole day cleared and starting working from that morning. I was very happy with how I did, and I learned many things along the way! Achieving 3rd place is also what led to me being offered a position working at BYU\'s Cybersecurity Research Lab.'
+    },
+    {
         question: 'What are your greatest strengths?',
-        answer: 'My greatest strengths are probably an attention to detail, being a natural leader, and a quick learner.'
+        answer: 'I would say my greatest strengths are probably an attention to detail, being a natural leader, and a quick learner. I think that one of the greatest keys to success (and something many employers are looking for) is initiative - you are expected to get things done without having to be commanded in every little task. Those three strengths help me to take initiative in my work.'
     },
     {
-        question: 'What is your greatest professional achievement?',
-        answer: 'My greatest professional achievement has been getting my last job working for BYU\'s Dept of Continuing Education Quality Assurance Team. Despite this being my first semester in college, I was able to land a well-paying job that allows me to grow my technical skills.'
+        question: 'What\'s your biggest weakness?',
+        answer: 'I think my biggest weakness is facing rejection - it sucks. I remember when I served as a missionary in Houston, Texas. I had a responsibility to share my beliefs with everyone who was willing to listen and learn, and there really weren\'t many of them! It was hard getting out each day, and I felt down and depressed a lot. That being said, I don\'t give up. I talked to the other missionaries about my struggles so we could support each other, and I tried different strategies to help me feel more positive and motivated to leave our apartment each morning. I can honestly say that I worked through the pain of rejection and was able to share my beliefs with many other people.'
     },
     {
-        question: 'Tell me one lesson you\'ve learned from life.',
-        answer: 'I served a two-year mission in Houston, Texas as a Spanish-speaking missionary. My time as a missionary has taught me to work hard regardless of any obstacles in my way. There were lots of people that I contacted who were not interested in my message, but I knew that I needed to keep pushing forward.'
-    }
+        question: 'Describe your dream job.',
+        answer: 'My dream job is to work in a cybersecurity-related position where I can help protect other people from various malicious actors. I think the most important part of my dream job is the environment - I idealize working with others who know and respect me; they know where my boundaries are, but can push me to be better. I would love to collaborate with coworkers who also believe in what they\'re doing and want to help others.'
+    },
+    {
+        question: 'What\'s the toughest decision you\'ve had to make in the last 6 months?',
+        answer: 'I had a roommate who I struggled to live with in my dorm. I really liked the guy, but there were some things he did that I couldn\'t handle, and even after talking about them together respectfully, he would eventually start again. I didn\'t want to hurt his feelings, but I knew that I needed a change. After talking with some close family members and friends, I decided that it would be in our best interest to change apartments. I would do better having a roommate who didn\'t have those habits, and he would do better if he had a roommate who was okay with them. It ended up being a peaceful resolution to what could have easily become a bitter battle.'
+    },
+    {
+        question: 'What do you like to do outside of work and school?',
+        answer: 'I spend a lot of my time outside of work and school learning new technical skills, such as password cracking, using ciphers, or studying for the CompTIA Security+ certification (you can see a list of my personal projects on the Skills page). When I\'m not doing that, I\'m spending time hanging out with family and friends!'
+    },
 ];
 
 export default data;
